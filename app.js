69c6cbc828356343e8cddfdf4149324957581b2d
Stan-Getz <stanislav.genunchi@gmail.com>
2021-08-23 14:15:18 +0200

    Change a typo

--- app.js
+++ app.js
@@ -1,22 +1,16 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
-const port = 4000
-
-
-
-
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const port = 4000;
 
 const app = express();
 
-app.use(cors({ origin: "*", exposedHeaders: "token" }));
+app.use(cors({ origin: '*', exposedHeaders: 'token' }));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-
-
 app.listen(port, () => {
-  console.log("====================================");
-  console.log("Server start with port: " + port);
-  console.log("====================================");
+  console.log('====================================');
+  console.log('Server start with port: ' + port);
+  console.log('====================================');
 });
