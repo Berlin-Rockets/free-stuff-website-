2f8c81fb087aa1984bae62aaea6fa64faff3c578
Stan-Getz <stanislav.genunchi@gmail.com>
2021-08-27 11:46:41 +0200

    Added more routes

--- routes/ItemRoutes.js
+++ routes/ItemRoutes.js
@@ -1,18 +1,22 @@
-const express = require("express");
+const express = require('express');
 const ItemRoute = express.Router();
 
 const {
-getAllItems,
-postItem,
-fItemByCategory
-} = require("../controllers/ItemController");
+  getAllItems,
+  getItem,
+  postItem,
+  editItem,
+  deleteItem,
+  fItemByCategory,
+  fItemByLocation,
+} = require('../controllers/ItemController');
 
-
-
-ItemRoute.get("/", getAllItems);
-ItemRoute.get("/category", fItemByCategory);
-ItemRoute.post("/", postItem);
-
-
+ItemRoute.get('/', getAllItems);
+ItemRoute.get('/:id', getItem);
+ItemRoute.patch('/:id', editItem);
+ItemRoute.delete('/:id', deleteItem);
+ItemRoute.get('/:filter', fItemByCategory);
+ItemRoute.get('/location/:city', fItemByLocation);
+ItemRoute.post('/', postItem);
 
 module.exports = ItemRoute;
