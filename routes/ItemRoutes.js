aef7aaad69134623b3e8829abdbd5793877cfc4b
Stan G <64552069+Stan-Getz@users.noreply.github.com>
2021-09-02 14:31:21 +0200

    Update ItemRoutes.js

--- routes/ItemRoutes.js
+++ routes/ItemRoutes.js
@@ -1,26 +1,31 @@
-const express = require('express');
-const ItemRoute = express.Router();
+ const express = require("express");
+ const ItemRoute = express.Router();
 
-const {
-  getAllItems,
-  getSingleItem,
-  postItem,
-  filterByOne,
-  filterItems,
-  // givenItem,
-  // neededItem,
-  editItem,
-  deleteItem,
-} = require('../controllers/ItemController');
+ const {
+ getAllItems,
+ getSingleItem,
+ postItem,
+ filterByOne,
+ filterItems,
+ givenItem,
+ neededItem,
+ editItem,
+ deleteItem
+ } = require("../controllers/ItemController");
 
-ItemRoute.get('/', getAllItems);
-ItemRoute.get('/:id', getSingleItem);
-ItemRoute.post('/', postItem);
-ItemRoute.get('/filter/:filter', filterByOne);
-ItemRoute.get('/filter/:filter/:both', filterItems);
-// ItemRoute.get('/given', givenItem);
-// ItemRoute.get('/items/needed', neededItem);
-ItemRoute.patch('/:id', editItem);
-ItemRoute.delete('/:id', deleteItem);
 
-module.exports = ItemRoute;
+
+ ItemRoute.get("/", getAllItems);
+ ItemRoute.get("/singleItem/:id",getSingleItem);
+ ItemRoute.post("/", postItem);
+ ItemRoute.get("/filter/:filter", filterByOne);
+ ItemRoute.get("/filter/:filter/:both", filterItems);
+ ItemRoute.get("/given", givenItem );
+ ItemRoute.get("/needed", neededItem);
+ ItemRoute.patch("/:id", editItem );
+ ItemRoute.delete("/:id", deleteItem);
+
+
+
+
+ module.exports = ItemRoute;
