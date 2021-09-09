8cec1376cf0923df0df6906027ee3680f6e7f874
Stan-Getz <stanislav.genunchi@gmail.com>
2021-09-09 13:57:19 +0200

    Added SingleItem and AllItems

---
+++ client/src/components/Categories/Data.js
@@ -0,0 +1,36 @@
+const data = [
+  {
+    label: 'Auto',
+    url: '/auto',
+  },
+  {
+    label: 'Electronics',
+    url: '/electronics',
+  },
+  {
+    label: 'Furniture',
+    url: '/furniture',
+  },
+  {
+    label: 'Fashion',
+    url: '/fashion',
+  },
+  {
+    label: 'Animals',
+    url: '/animals',
+  },
+  {
+    label: 'Garden',
+    url: '/garden',
+  },
+  {
+    label: 'Free Time',
+    url: '/free-time',
+  },
+  {
+    label: 'Misc',
+    url: '/misc',
+  },
+];
+
+export default data;
