c5998283896810fc62a46b86a5a94c91997854e4
Nath <nathaly@nathaly.net.br>
2021-09-09 13:36:09 +0200

    Remove some stuff and add PostItem folder

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,18 @@
 import React from "react";
 import "./App.css";
-import Banner from "./components/Banner";
 import Navbar from "./components/Navbar/Navbar";
+import Banner from "./components/Banner";
 import Footer from "./components/Footer";
+import UserForm from "./components/PostItem/UserForm";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <Banner />
-      <Footer />
+      <UserForm />
+      {/* <Navbar /> */}
+      {/* <Banner /> */}
+      {/* <PostItem /> */}
+      {/* <Footer /> */}
     </div>
   );
 }
---
+++ client/src/components/PostItem/Confirm.js
@@ -0,0 +1,65 @@
+import React, { Component } from "react";
+import Dialog from "@material-ui/core/Dialog";
+import AppBar from "@material-ui/core/AppBar";
+import { ThemeProvider as MuiThemeProvider } from "@material-ui/core/styles";
+import { List, ListItem, ListItemText } from "@material-ui/core/";
+import Button from "@material-ui/core/Button";
+
+export class Confirm extends Component {
+  continue = (e) => {
+    e.preventDefault();
+    // PROCESS FORM //
+    this.props.nextStep();
+  };
+
+  back = (e) => {
+    e.preventDefault();
+    this.props.prevStep();
+  };
+
+  render() {
+    const {
+      values: { name, description, categories, condition, interest, photo },
+    } = this.props;
+    return (
+      <MuiThemeProvider>
+        <>
+          <Dialog open fullWidth maxWidth="sm">
+            <AppBar title="Confirm Item Details" />
+            <List>
+              <ListItem>
+                <ListItemText primary="Item Name" secondary={name} />
+              </ListItem>
+              <ListItem>
+                <ListItemText primary="Description" secondary={description} />
+              </ListItem>
+              <ListItem>
+                <ListItemText primary="Category" secondary={categories} />
+              </ListItem>
+              <ListItem>
+                <ListItemText primary="Condition" secondary={condition} />
+              </ListItem>
+              <ListItem>
+                <ListItemText primary="Interest" secondary={interest} />
+              </ListItem>
+              <ListItem>
+                <ListItemText primary="Photo" secondary={photo} />
+              </ListItem>
+            </List>
+            <br />
+
+            <Button color="secondary" variant="contained" onClick={this.back}>
+              Back
+            </Button>
+
+            <Button color="primary" variant="contained" onClick={this.continue}>
+              Confirm & Continue
+            </Button>
+          </Dialog>
+        </>
+      </MuiThemeProvider>
+    );
+  }
+}
+
+export default Confirm;
