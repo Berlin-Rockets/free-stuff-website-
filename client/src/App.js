7f7bfde514763ee0062c0fcc9fba54379e91a7de
Nath <nathaly@nathaly.net.br>
2021-09-13 11:06:58 +0200

    Remove duplicate styling in App.css

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,27 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Banner from "./components/Banner";
+import Navbar from "./components/Navbar/Navbar";
 
-import React, { useState, useEffect } from 'react';
-import './App.css';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import Banner from './components/Banner';
-import Navbar from './components/Navbar/Navbar';
-
-import Footer from './components/Footer';
-import Login from './components/Login';
-import Register from './components/Register';
-import Categories from './components/Categories/Categories';
-import SingleItem from './components/SingleItem/SingleItem';
-import AllItems from './components/AllItems/AllItems';
-import axios from 'axios';
-import baseURL from '../src/config/baseUrl';
+import Footer from "./components/Footer";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import Categories from "./components/Categories/Categories";
+import SingleItem from "./components/SingleItem/SingleItem";
+import AllItems from "./components/AllItems/AllItems";
+import axios from "axios";
+import baseURL from "../src/config/baseUrl";
 import Accordion from "./components/Accordion/Accordion";
 import UserForm from "./components/";
-
 
 function App() {
   const [user, setUser] = useState();
 
   const getUser = async () => {
-    const userId = localStorage.getItem('userId');
+    const userId = localStorage.getItem("userId");
     if (userId) {
-      const userData = await axios.get(baseURL + '/users/' + userId);
+      const userData = await axios.get(baseURL + "/users/" + userId);
       console.log(userData.data);
       setUser(userData.data);
     }
@@ -33,27 +31,24 @@ function App() {
   }, []);
 
   return (
-
     <BrowserRouter>
       <div className="App">
-        <Navbar user={user} />
-
+        {/* <Navbar user={user} /> */}
         {/* <Banner /> */}
         {/* <SingleItem /> */}
- <UserForm />
-        <AllItems />
+        <UserForm />
+        {/* <AllItems /> */}
 
         <Switch>
-          <Route exact path="/banner" component={Banner} />
-          <Route exact path="/login" component={Login} />
-          <Route exact path="/register" component={Register} />
+          {/* <Route exact path="/banner" component={Banner} /> */}
+          {/* <Route exact path="/login" component={Login} /> */}
+          {/* <Route exact path="/register" component={Register} /> */}
           {/* <Route exact path="/singleItem/:id" component={SingleItem} /> */}
         </Switch>
         <Categories />
         <Footer />
       </div>
     </BrowserRouter>
-
   );
 }
 
