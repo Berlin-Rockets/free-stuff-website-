50355dc3770ecb0600e8596ddc6a7c2dd52cbbc7
Stan-Getz <stanislav.genunchi@gmail.com>
2021-09-15 14:34:10 +0200

    Changed the style

--- client/src/components/Accordion/Accordion.js
+++ client/src/components/Accordion/Accordion.js
@@ -1,30 +1,32 @@
-import { useState } from "react";
-import { faqs } from "./Data";
-import AccordionItem from "./AccordionItem";
-import "./Accordion.css";
+import { useState } from 'react';
+import { faqs } from './Data';
+import AccordionItem from './AccordionItem';
+import './Accordion.css';
 
 const Accordion = () => {
-  const [clicked, setClicked] = useState("0");
+  const [clicked, setClicked] = useState('0');
 
   const handleToggle = (index) => {
     if (clicked === index) {
-      return setClicked("0");
+      return setClicked('0');
     }
     setClicked(index);
   };
 
   return (
-    <ul className="accordion">
+    <div className="container">
       <h1 className="heading">Frequently Asked Questions</h1>
-      {faqs.map((faq, index) => (
-        <AccordionItem
-          onToggle={() => handleToggle(index)}
-          active={clicked === index}
-          key={index}
-          faq={faq}
-        />
-      ))}
-    </ul>
+      <ul className="accordion">
+        {faqs.map((faq, index) => (
+          <AccordionItem
+            onToggle={() => handleToggle(index)}
+            active={clicked === index}
+            key={index}
+            faq={faq}
+          />
+        ))}
+      </ul>
+    </div>
   );
 };
 
