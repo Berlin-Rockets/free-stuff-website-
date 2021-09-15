49594276a2abe62a0cbffac3147966489aad1880
Stan-Getz <stanislav.genunchi@gmail.com>
2021-09-15 14:33:42 +0200

    Just commented the components out

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,29 @@
-import React, { useState, useEffect } from "react";
-import "./App.css";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
-import Banner from "./components/Banner";
-import Navbar from "./components/Navbar/Navbar";
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Banner from './components/Banner';
+import Navbar from './components/Navbar/Navbar';
 
-import Footer from "./components/Footer";
-import Login from "./components/Login";
-import Register from "./components/Register";
-import Categories from "./components/Categories/Categories";
-import SingleItem from "./components/SingleItem/SingleItem";
-import AllItems from "./components/AllItems/AllItems";
-import axios from "axios";
-import baseURL from "../src/config/baseUrl";
-import Accordion from "./components/Accordion/Accordion";
-import UserForm from "./components/PostItem/UserForm";
-import Carousel from "./components/Carousel";
+import Footer from './components/Footer';
+import Login from './components/Login';
+import Register from './components/Register';
+import Categories from './components/Categories/Categories';
+import SingleItem from './components/SingleItem/SingleItem';
+import AllItems from './components/AllItems/AllItems';
+import axios from 'axios';
+import baseURL from '../src/config/baseUrl';
+import Accordion from './components/Accordion/Accordion';
+// import UserForm from './components/PostItem/UserForm';
+import Carousel from './components/Carousel';
+import Support from './components/Support/Support';
 
 function App() {
   const [user, setUser] = useState();
 
   const getUser = async () => {
-    const userId = localStorage.getItem("userId");
+    const userId = localStorage.getItem('userId');
     if (userId) {
-      const userData = await axios.get(baseURL + "/users/" + userId);
+      const userData = await axios.get(baseURL + '/users/' + userId);
       console.log(userData.data);
       setUser(userData.data);
     }
@@ -35,27 +36,21 @@ function App() {
     <BrowserRouter>
       <div className="App">
         <Navbar user={user} />
-<<<<<<< HEAD
-
-        <Banner />
-        {/* <Categories /> */}
-        {/* <SingleItem /> */}
-        {/* <UserForm />  */}
-=======
-        <Banner />
+        {/* <Banner /> */}
+        <Support />
         <Categories />
         {/* <SingleItem /> */}
-        <UserForm />
+        {/* <UserForm /> */}
         {/* <AllItems /> */}
->>>>>>> 65b57ab183c7d62e99f658baca8fc572c4a15cac
-        <Carousel />
+        {/* <Accordion /> */}
+        {/* <Carousel /> */}
 
         <Switch>
           <Route exact path="/banner" component={Banner} />
           <Route exact path="/login" component={Login} />
           <Route exact path="/register" component={Register} />
           <Route exact path="/allitems" component={AllItems} />
-          <Route exact path="/" component={UserForm} />
+          {/* <Route exact path="/" component={UserForm} /> */}
           <Route exact path="/carousel" component={Carousel} />
 
           {/* <Route exact path="/singleItem/:id" component={SingleItem} /> */}
