eb48f804856ff3d989ab2f5e965f9a5d8a516e84
Stan-Getz <stanislav.genunchi@gmail.com>
2021-09-29 22:50:34 +0200

    Completely redesigned Register Page

--- client/src/components/Register/Register.js
+++ client/src/components/Register/Register.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import baseURL from '../../config/baseUrl';
 import './Register.css';
+import '../Login/Login.css';
 // import { FcGoogle } from "react-icons/fc";
 // import { FaFacebook } from "react-icons/fa";
 
@@ -46,7 +47,7 @@ export default function Register() {
   };
 
   return (
-    <div className="register d-flex  flex-column align-items-center  height rounded">
+    <div className="register-container d-flex align-items-center mx-0">
       {error && (
         <div className="alert alert-danger" role="alert">
           {error}
@@ -57,123 +58,106 @@ export default function Register() {
           {success}
         </div>
       )}
-      <form className="row flex mt-2 " onSubmit={registerHandler}>
-        <div className="col-12 ">
-          <div className="d-flex my-1 ">
-            <div className="col-6 ">
-              <label className="form-label" htmlFor="inputUsername">
-                name
-              </label>
-              <input
-                className="form-control m-1 "
-                id="inputUsername"
-                type="text"
-                name="name"
-                required
-              />
-            </div>
-
-            <div className="col-6 ">
-              <label htmlFor="inputEmail4" className="form-label">
-                Email
-              </label>
-              <input
-                className="form-control border"
-                id="inputEmail4"
-                type="email"
-                name="email"
-                required
-              />
-            </div>
+      <div className="card-container-reg card">
+        <div className="card-header header-style text-center">
+          <h5 className="pt-2">Register with us!</h5>
+        </div>
+        <div className="card-body">
+          <div className="card-title mt-3">
+            Please fill out the fields below:
           </div>
-
-          <div className="d-flex ">
-            <div className="col-6">
-              <label className="form-label" htmlFor="inputUsername">
-                Phone
-              </label>
+          <form className="d-flex flex-column" onSubmit={registerHandler}>
+            <div className="container p-0 d-flex flex-column flex-wrap">
+              <div className="container p-0 d-flex justify-content-between">
+                <input
+                  className="form-control mb-3 input-style"
+                  id="inputUsername"
+                  type="text"
+                  name="name"
+                  placeholder="Username*"
+                  required
+                />
+                <input
+                  className="form-control border mb-3 mt-0 input-style"
+                  id="inputEmail4"
+                  type="email"
+                  name="email"
+                  placeholder="Email*"
+                  required
+                />
+              </div>
+              <div className="container p-0 d-flex">
+                <input
+                  className="form-control mb-3 input-style"
+                  id="inputPassword4"
+                  type="password"
+                  name="password"
+                  placeholder="Password*"
+                  required
+                />
+                <input
+                  className="form-control mb-3 input-style"
+                  id="inputPassword4"
+                  type="password"
+                  name="confirmPassword"
+                  placeholder="Confirm Password*"
+                  required
+                />
+              </div>
               <input
-                className="form-control "
+                className="form-control mb-3 input-style"
                 id="inputUsername"
                 type="text"
                 name="phone"
+                placeholder="Phone (optional)"
                 required
               />
             </div>
 
-            <div className="col-6">
-              <label className="form-label" htmlFor="inputPassword4">
-                Password
-              </label>
+            <div className="form-check mt-2">
               <input
-                className="form-control"
-                id="inputPassword4"
-                type="password"
-                name="password"
+                className="form-check-input"
+                type="checkbox"
+                value=""
+                id="flexCheckDefault"
                 required
               />
+              <label className="form-check-label" for="flexCheckDefault">
+                Accept our Terms and Conditions.
+              </label>
             </div>
-          </div>
-
-          <div className="col-6 mt-2">
-            <label className="form-label" htmlFor="inputPassword4">
-              Confirm Password
-            </label>
+            <div className="form-check mt-2">
+              <input
+                className="form-check-input"
+                type="checkbox"
+                value=""
+                id="flexCheckDefault"
+              />
+              <label className="form-check-label" for="flexCheckDefault">
+                Do you wish to receive our newsletter? You can cancel it at any
+                time.
+              </label>
+            </div>
+            <div className="form-check mt-2 w-100">
+              <input
+                className="form-check-input"
+                type="checkbox"
+                value=""
+                id="flexCheckDefault"
+                required
+              />
+              <label className="form-check-label" for="flexCheckDefault">
+                I am not a robot - reCAPTCHA
+              </label>
+            </div>
             <input
-              className="form-control"
-              id="inputPassword4"
-              type="password"
-              name="confirmPassword"
-              required
+              className="btn btn-dark w-50 mt-4 mx-auto text-capitalize"
+              type="submit"
+              value="Register"
             />
-          </div>
-
-          <div className="form-check mt-2">
-            <input
-              className="form-check-input"
-              type="checkbox"
-              value=""
-              id="flexCheckDefault"
-              required
-            />
-            <label className="form-check-label" for="flexCheckDefault">
-              Accept our Terms and Conditions.
-            </label>
-          </div>
-
-          <div className="form-check mt-2">
-            <input
-              className="form-check-input"
-              type="checkbox"
-              value=""
-              id="flexCheckDefault"
-            />
-            <label className="form-check-label" for="flexCheckDefault">
-              Do you wish to receive our newsletter? You can cancel it at any
-              time.
-            </label>
-          </div>
-
-          <div className="form-check mt-2 w-100">
-            <input
-              className="form-check-input"
-              type="checkbox"
-              value=""
-              id="flexCheckDefault"
-              required
-            />
-            <label className="form-check-label" for="flexCheckDefault">
-              I am not a robot - reCAPTCHA
-            </label>
-          </div>
-
-          <input
-            className="btn btn-primary w-100 mt-5"
-            type="submit"
-            value="Register"
-          />
+          </form>
         </div>
-      </form>
+      </div>
     </div>
   );
 }
