01c3aa460725c39d6a3f787a3c5c85c6fbc1aeac
Stan-Getz <stanislav.genunchi@gmail.com>
2021-09-29 20:33:33 +0200

    Styled the Search Button

--- client/src/components/Searchbar/Searchbar.js
+++ client/src/components/Searchbar/Searchbar.js
@@ -1,76 +1,66 @@
-import React, { useState, useEffect } from "react";
-import "react-bootstrap";
-import "./Searchbar.css";
-import { Button } from "./../Navbar/Button";
+import React, { useState, useEffect } from 'react';
+import 'react-bootstrap';
+import './Searchbar.css';
+import { Button } from '../Button/Button';
 import baseURL from '../../config/baseUrl';
 import axios from 'axios';
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 export default function Searchbar() {
-const [data, setData] = useState([])
-const [filteredData, setFilteredData]= useState([])
+  const [data, setData] = useState([]);
+  const [filteredData, setFilteredData] = useState([]);
 
-const getAllItems = async () => {
-  try {
-    const res = await axios.get(baseURL + '/items');
-    console.log('searchbar dataaaa',res.data.data);
-    setData(res.data.data)
-  } catch (e) {
-    console.log(e);
-  }
-};
+  const getAllItems = async () => {
+    try {
+      const res = await axios.get(baseURL + '/items');
+      console.log('searchbar dataaaa', res.data.data);
+      setData(res.data.data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
 
-useEffect(() => {
-  getAllItems();
-  
-}, []);
+  useEffect(() => {
+    getAllItems();
+  }, []);
 
-const handleFilter = (e) => {
-  const searchWord = e.target.value 
-  const newFilter = data.filter((value)=>{
-    return value.name.toLowerCase().includes(searchWord.toLowerCase())
-  })
-  if (searchWord === '') {
-    setFilteredData([])
-  }else{
-    setFilteredData(newFilter)
-  }
- 
- } 
+  const handleFilter = (e) => {
+    const searchWord = e.target.value;
+    const newFilter = data.filter((value) => {
+      return value.name.toLowerCase().includes(searchWord.toLowerCase());
+    });
+    if (searchWord === '') {
+      setFilteredData([]);
+    } else {
+      setFilteredData(newFilter);
+    }
+  };
 
-    return (
-     <div>
-        
-          <div className="mb-3 col-sm-9 pl-0 pr-2">
-            <div className="input-group">
-              <input
-                type="text"
-                className="form-control py-2"
-                placeholder="Type here what you are looking for"
-                onChange={handleFilter}
-              />
-              <Button className="btn">Search</Button>
-            </div>
-{filteredData && filteredData.length !== 0 && (
-  <div className='dataResult'>
-  {filteredData.slice(0,5).map((value,key)=>{
-    return(
-      <Link
-      to={"items/singleItem/" + value._id}
-      className='dataItem'
-    > <p>{value.name}</p></Link>
-     
-    )
-  })}
-</div>
-)}
-          
-
-          </div>
-      
+  return (
+    <div>
+      <div className="mb-3 col-sm-9 pl-0 pr-2">
+        <div className="input-group">
+          <input
+            type="text"
+            className="form-control py-2"
+            placeholder="Type here what you are looking for"
+            onChange={handleFilter}
+          />
+          <div className="btn--search">Search</div>
         </div>
-    );
- 
+        {filteredData && filteredData.length !== 0 && (
+          <div className="dataResult">
+            {filteredData.slice(0, 5).map((value, key) => {
+              return (
+                <Link to={'items/singleItem/' + value._id} className="dataItem">
+                  {' '}
+                  <p>{value.name}</p>
+                </Link>
+              );
+            })}
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
-
-
