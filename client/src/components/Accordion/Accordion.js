8ede9ee44425251426a883a749c7c4623ba3434c
Stan-Getz <stanislav.genunchi@gmail.com>
2021-09-30 11:21:03 +0200

    Fixed border-radius

--- client/src/components/Accordion/Accordion.js
+++ client/src/components/Accordion/Accordion.js
@@ -1,14 +1,14 @@
-import { useState } from "react";
-import { faqs } from "./Data";
-import AccordionItem from "./AccordionItem";
-import "./Accordion.css";
+import { useState } from 'react';
+import { faqs } from './Data';
+import AccordionItem from './AccordionItem';
+import './Accordion.css';
 
 const Accordion = () => {
-  const [clicked, setClicked] = useState("0");
+  const [clicked, setClicked] = useState('0');
 
   const handleToggle = (index) => {
     if (clicked === index) {
-      return setClicked("0");
+      return setClicked('0');
     }
     setClicked(index);
   };
@@ -19,7 +19,7 @@ const Accordion = () => {
         <h4>
           <span>Frequently Asked Questions</span>
         </h4>
-        <ul className="accordion">
+        <ul className="accordion m-0 p-0">
           {faqs.map((faq, index) => (
             <AccordionItem
               onToggle={() => handleToggle(index)}
