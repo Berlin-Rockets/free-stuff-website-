b991b2028ff9595c7c2ffe4caf5c084ec7a94828
Ahmad <ahhk.48@gmail.com>
2021-10-18 11:40:07 +0200

    update the code

--- app.js
+++ app.js
@@ -27,8 +27,8 @@ app.use("/api/items", itemRoute);
 
 if (process.env.NODE_ENV === "production") {
   // Set static folder
-  app.use(express.static("client/build"));
-
+  // app.use(express.static("client/build"));
+  app.use(express.static(path.join(__dirname, "client/build")));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "/client", "/build", "/index.html"));
   });
