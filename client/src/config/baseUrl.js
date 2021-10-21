2aca58274c793038979aac25f3f3158b1315efa9
Ahmad <ahhk.48@gmail.com>
2021-10-21 15:10:11 +0200

    update

--- client/src/config/baseUrl.js
+++ client/src/config/baseUrl.js
@@ -1,2 +1,9 @@
 const baseURL= 'https://freebay-free-stuff.herokuapp.com'
 export default baseURL
+
+// const baseURL =
+//   process.env.REACT_APP_ENV === "development"
+//     ? "http://localhost:5000/items"
+//     : "https://grocery-shopping-list-app.herokuapp.com/items";
+
+// export default baseURL;
